220c8c3abd13e33b21ee3d6104f92b1d2afea803
AdityaGuturu <151147156+AdityaGuturu@users.noreply.github.com>
2024-10-17 12:32:32 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -6,10 +6,6 @@ const app = express()
 const address = path.join(__dirname,"/public")
 app.set("view engine","ejs")
 app.use(express.static(address))
-const https = require('https');
-
-https.get('https://your-service-url.com', { rejectUnauthorized: false }, (res) => {
-});
 
 app.get("/car",async(req,resp)=>{
     const connection = await DB2();
@@ -18,8 +14,4 @@ app.get("/car",async(req,resp)=>{
     resp.render("car",{data})
 })
 
-const port = process.env.PORT || 10000;  // Use the port from the environment
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(10000});
